2384924d7500f69416f6ec1df1bf47a48de423d5
Shubham Palriwala <spalriwalau@gmail.com>
2021-01-15 18:31:46 +0530

    fix: add-auth-in-key-routes

--- src/routes/key.ts
+++ src/routes/key.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import passport from "passport";
 import KeysController from "../controllers/key";
 
 const keysRouter = Router();
 const keysController = new KeysController();
 
+const userAuthMiddleware = passport.authenticate("userStrategy", {
+  session: false,
+});
+
 keysRouter.get("/all", keysController.fetchProjectKeys);
 keysRouter.get("/", keysController.fetchKey);
-keysRouter.post("/", keysController.createKey);
-keysRouter.put("/", keysController.updateKey);
-keysRouter.delete("/", keysController.deleteKey);
+keysRouter.post("/", userAuthMiddleware, keysController.createKey);
+keysRouter.put("/", userAuthMiddleware, keysController.updateKey);
+keysRouter.delete("/", userAuthMiddleware, keysController.deleteKey);
 
 export default keysRouter;
